982c42da9a1ac3ed45ea80ee1fe4dd5120886f0b
alexrybrown <alexrybrown@gmail.com>
2017-03-25 12:02:55 -0600

    Added better names for orm.

--- assignments/models.py
+++ assignments/models.py
@@ -6,8 +6,14 @@ class Assignment(models.Model):
     questions = models.ManyToManyField('assignments.Question', blank=True)
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Question(models.Model):
     game_type = models.ForeignKey('games.Game')
     question = models.CharField(max_length=100)
     answer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{}={}".format(self.question, self.answer)
