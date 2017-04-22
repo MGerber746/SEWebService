1c182f9135c20ae611cba6ef7d41257afdd41c1f
alexrybrown <alexrybrown@gmail.com>
2017-04-22 12:25:29 -0600

    added more url routes

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -28,10 +28,11 @@ class AccountSerializer(serializers.ModelSerializer):
         # the password will be stored in plain text.
         user = User.objects.create(
             username=validated_data['username'],
-            email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
         )
+        if validated_data.get('email'):
+            user.email = validated_data['email']
         user.set_password(validated_data['password'])
         user.save()
         # After saving the user, create a token for that user.
@@ -45,9 +46,26 @@ class StudentSerializer(serializers.ModelSerializer):
         fields = '__all__'
         depth = 1
 
+    def validate(self, attrs):
+        request = self.context.get('request')
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            attrs['user'] = user
+        else:
+            raise serializers.ValidationError("User wasn't created")
+        return attrs
+
+    def create(self, validated_data):
+        student = super(StudentSerializer, self).create(validated_data)
+        request = self.context.get('request')
+        teacher = account_models.Teacher.objects.get(user=request.user)
+        teacher.students.add(student)
+        return student
+
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = account_models.Teacher
         fields = '__all__'
-        depth = 1
+        depth = 2
--- classes/views.py
+++ classes/views.py
@@ -7,3 +7,6 @@ from classes.serializers import ClassSerializer
 class ClassViewSet(viewsets.ModelViewSet):
     queryset = Class.objects.all()
     serializer_class = ClassSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
